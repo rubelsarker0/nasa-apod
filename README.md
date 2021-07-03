8097d51ffd12cbcf968517fc85b57c02d55dd677
Rubel Sarker <rubel_sarker@dellteam.com>
2021-07-03 22:35:15 +0100

    Added the apod functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nasa-apod
+
+**This vanilla javascript application will able to fetch the data from NASA apod API and can display the picture of the day with explanation and the date of the image**
---
+++ js/apod.js
@@ -0,0 +1,27 @@
+const url = 'https://api.nasa.gov/planetary/apod?api_key=';
+const apiKey = config.NASA_API_KEY;
+
+const fetchNasaApodData = async () => {
+	try {
+		const response = await fetch(`${url}${apiKey}`);
+		const data = await response.json();
+		displayData(data);
+	} catch (error) {
+		console.log(`Data not found ${error}`);
+	}
+};
+
+const displayData = (data) => {
+	const imageOfTheDay = document.querySelector('.image-of-the-day');
+	const imageTitle = document.querySelector('.image-title');
+	const imageExplanation = document.querySelector('.image-explanation');
+	const imageDate = document.querySelector('.image-date');
+
+	imageOfTheDay.src = data.hdurl;
+	imageTitle.textContent = data.title;
+	imageExplanation.textContent = data.explanation;
+	imageDate.textContent = `Date of the image: ${data.date}`;
+	console.log(imageOfTheDay, imageTitle, imageExplanation, imageDate);
+};
+
+fetchNasaApodData();
